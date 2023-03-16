5132c6e2e98f79eb8682af9c845e1b0fd6aec56c
Enkhmandakh <Enkhmandakh@Buynkhishigs-iMac.local>
2023-03-16 17:39:59 +0800

    page ntr neesen hiiheer bol project deeree neree bicheed progressruugee oruulaarai

--- front-end/src/pages/home-page.js
+++ front-end/src/pages/home-page.js
@@ -1,4 +1,10 @@
 import "../styles/home-page.css";
+import Triceratops from "../photos/Triceratops.jpeg";
+import Spinosaurus from "../photos/spinosaurus.jpeg";
+import Stegosaurus from "../photos/stegosaurus.jpeg";
+import Tarbosaurus from "../photos/tarbosaurus.jpeg";
+import Tyrannosaurus from "../photos/Tyrannosaurus.webp";
+import Velociraptor from "../photos/Velociraptor.jpeg";
 
 export const Homepage = () => {
   return (
@@ -14,61 +20,72 @@ export const Homepage = () => {
       </div>
 
       <div className="page1">
-        <div className="Welcome">Start exploring dinosaurs</div>
-        <div className="example">
-          <img alt="Tyra-img" className="Tyra-img"></img>
-          <div className="Tyrannosaurus">Tyrannosaurus</div>
-          <div className="Tyra-about">
-            Тираннозавр махчин амьтан байсан. Цэрдийн галавын үед амьдарч, Хойд
-            Америкт амьдарч байжээ. Түүний олдворыг Utah, Coahuila (Mexico),
-            Montana зэрэг газраас олжээ.
+
+        <div className="introduction">
+          <div className="Welcome">Start exploring dinosaurs</div>
+          <div className="Welcome-text">
+            A friendly yet encyclopedic guide to the some of the most incredible
+            animals to ever walk the Earth, the Dinosaurs.
           </div>
         </div>
-        <div className="example">
-          <img alt="Tarbo-img" className="Tarbo-img"></img>
-          <div className="Tyrannosaurus">Tarbosaurus bataar</div>
-          <div className="Tarbo-about">
-            Тарбозавр махчин амьтан байв. Цэрдийн галавын үед амьдарч, Ази тивд
-            амьдарч байжээ. Түүний олдворыг Hubei (China), Баянхонгор
-            (Mongolia), Ömnögovi (Mongolia) зэрэг газраас олжээ.
+
+        <div className="inside-page1">
+          <div className="example">
+            <img src={Tyrannosaurus} alt="Tyra-img" className="Tyra-img"></img>
+            <div className="Tyrannosaurus">Tyrannosaurus</div>
+            <div className="Tyra-about">
+              Тираннозавр махчин амьтан байсан. Цэрдийн галавын үед амьдарч,
+              Хойд Америкт амьдарч байжээ. Түүний олдворыг Utah, Coahuila
+              (Mexico), Montana зэрэг газраас олжээ.
+            </div>
+          </div>
+          <div className="example">
+            <img src={Tarbosaurus} alt="Tarbo-img" className="Tarbo-img"></img>
+            <div className="Tarbosaurus">Tarbosaurus bataar</div>
+            <div className="Tarbo-about">
+              Тарбозавр махчин амьтан байв. Цэрдийн галавын үед амьдарч, Ази
+              тивд амьдарч байжээ. Түүний олдворыг Hubei (China), Баянхонгор
+              (Mongolia), Ömnögovi (Mongolia) зэрэг газраас олжээ.
+            </div>
+          </div>
+          <div className="example">
+            <img src={Spinosaurus} alt="Spino-img" className="Spino-img"></img>
+            <div className="Spinosaurus">Spinosaurus</div>
+            <div className="Spino-about">
+              Спинозавр махчин амьтан байсан. Цэрдийн галавын үед амьдарч,
+              Африкт амьдарч байжээ. Түүний олдворыг Tataouine (Tunisia), Adrar
+              (Algeria), Béchar (Algeria) зэрэг газраас олжээ.
+            </div>
+          </div>
+          <div className="example">
+            <img src={Triceratops} alt="Trice-img" className="Trice-img"></img>
+            <div className="Triceratops">Triceratops</div>
+            <div className="Trice-about">
+              Triceratops өвсөн тэжээлтэн байсан. Цэрдийн галавын үед амьдарч,
+              Хойд Америкт амьдарч байжээ. Түүний олдворуудыг Хойд Дакота,
+              Вайоминг, Саскачеван (Канад) зэрэг газраас олжээ.
+            </div>
+          </div>
+          <div className="example">
+            <img src={Velociraptor} alt="Velo-img" className="Velo-img"></img>
+            <div className="Velociraptor">Velociraptor</div>
+            <div className="Velo-about">
+              Velociraptor махчин амьтан байсан. Цэрдийн галавын үед амьдарч,
+              Ази тивд амьдарч байжээ. Түүний олдворыг Alberta (Canada), Inner
+              Mongolia (China), Batken Region (Kyrgyzstan) зэрэг газраас олжээ.
+            </div>
+          </div>
+          <div className="example">
+            <img src={Stegosaurus} alt="Stego-img" className="Stego-img"></img>
+            <div className="Stegosaurus">Stegosaurus</div>
+            <div className="Stego-about">
+              Stegosaurus бол өвсөн тэжээлтэн байсан. Тэрээр Юрийн галавын үед
+              амьдарч, Европ, Хойд Америкт амьдарч байжээ. Түүний олдворыг Utah,
+              Xinjiang (China), South Dakota зэрэг газраас олжээ.
+            </div>
           </div>
         </div>
-        <div className="example">
-          <img alt="Spino-img" className="Spino-img"></img>
-          <div className="Spinosaurus">Spinosaurus</div>
-          <div className="Spino-about">
-            Спинозавр махчин амьтан байсан. Цэрдийн галавын үед амьдарч, Африкт
-            амьдарч байжээ. Түүний олдворыг Tataouine (Tunisia), Adrar
-            (Algeria), Béchar (Algeria) зэрэг газраас олжээ.
-          </div>
-        </div>
-        <div className="example">
-          <img alt="Trice-img" className="Trice-img"></img>
-          <div className="Triceratops">Triceratops</div>
-          <div className="Trice-about">
-            Triceratops өвсөн тэжээлтэн байсан. Цэрдийн галавын үед амьдарч,
-            Хойд Америкт амьдарч байжээ. Түүний олдворуудыг Хойд Дакота,
-            Вайоминг, Саскачеван (Канад) зэрэг газраас олжээ.
-          </div>
-        </div>
-        <div className="example">
-          <img alt="Velo-img" className="Velo-img"></img>
-          <div className="Velociraptor">Velociraptor</div>
-          <div className="Velo-about">
-            Velociraptor махчин амьтан байсан. Цэрдийн галавын үед амьдарч, Ази
-            тивд амьдарч байжээ. Түүний олдворыг Alberta (Canada), Inner
-            Mongolia (China), Batken Region (Kyrgyzstan) зэрэг газраас олжээ.
-          </div>
-        </div>
-        <div className="example">
-          <img alt="Stego-img" className="Stego-img"></img>
-          <div className="Stegosaurus">Stegosaurus</div>
-          <div className="Stego-about">
-            Stegosaurus бол өвсөн тэжээлтэн байсан. Тэрээр Юрийн галавын үед
-            амьдарч, Европ, Хойд Америкт амьдарч байжээ. Түүний олдворыг Utah,
-            Xinjiang (China), South Dakota зэрэг газраас олжээ.
-          </div>
-        </div>
+        
       </div>
     </div>
   );
